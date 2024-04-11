b3043b06132f376e59eb59814b0f4a8778bbb1ce
koneck <101557841+koneck@users.noreply.github.com>
2024-04-11 15:50:33 +0300

    Add files via upload

---
+++ Variables var, let, const/var_let_const_call_3.js
@@ -0,0 +1,8 @@
+console.log(x_var); // var допускает но выведет undefined остальные выведут ошибку
+var x_var = 10;
+
+console.log(x_let); 
+let x_let = 20;
+
+console.log(x_const); 
+const x_const = 30;
---
+++ Variables var, let, const/var_let_const_declare_2.js
@@ -0,0 +1,16 @@
+var x_var = 1;
+let x_let = 2;
+const x_const = 3;
+
+//var позволяет повторно объявлять
+//let не позволяет
+//const не позволяет
+var x_var = 4; 
+console.log(x_var); 
+
+let x_let = 5; 
+console.log(x_let); 
+
+const x_const = 6; 
+console.log(x_const); 
+
